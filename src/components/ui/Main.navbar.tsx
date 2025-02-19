6d94b89c1402308de26c88c376621c1ff62a0267
Mikael Aditya N <mikaeladityan.99@gmail.com>
2025-02-19 10:20:21 +0700

    Update: Redirect to router.push

--- src/components/ui/Main.navbar.tsx
+++ src/components/ui/Main.navbar.tsx
@@ -1,16 +1,17 @@
 'use client';
 import { IconX } from '@tabler/icons-react';
 import Image from 'next/image';
-import { redirect } from 'next/navigation';
+import { useRouter } from 'next/navigation';
 import { Steps } from '../Steps';
 
 export function MainNavbar() {
+    const router = useRouter();
     return (
         <>
             <nav className="w-full max-w-screen-2xl z-50 relative">
                 <div className="flex items-center justify-between text-white gap-5 bg-brown py-6 px-12">
                     <Image src={'/images/logo.svg'} alt="Logo Wow Borneo" priority width={200} height={200} className="max-w-32" />
-                    <button onClick={() => redirect(`${process.env.NEXT_PUBLIC_HOME}`)}>
+                    <button type="button" onClick={() => router.push(`${process.env.NEXT_PUBLIC_HOME}`)}>
                         <IconX size={35} stroke={2} />
                     </button>
                 </div>
--- src/components/ui/Services/Content.service.tsx
+++ src/components/ui/Services/Content.service.tsx
@@ -1,7 +1,9 @@
 import { Service } from '@/types';
 import Image from 'next/image';
+import { useRouter } from 'next/navigation';
 
 export function ContentService({ service }: { service: Service[] }) {
+    const router = useRouter();
     return service.map((service, i) => (
         <div className={`p-6 ${i % 2 === 0 ? 'bg-brown/10' : 'bg-brown/5'} w-full flex items-center justify-between gap-6`} key={i}>
             <div className="flex items-center justify-start gap-3">
@@ -19,7 +21,7 @@ export function ContentService({ service }: { service: Service[] }) {
                 </div>
             </div>
 
-            <button className="py-3 px-6 rounded-2xl bg-white text-black font-bold text-center text-[20px]">
+            <button type="button" onClick={() => router.push('/booking-itinerary')} className="py-3 px-6 rounded-2xl bg-white text-black font-bold text-center text-[20px]">
                 Start ${service.price}/ {service.days}/Pax
             </button>
         </div>
